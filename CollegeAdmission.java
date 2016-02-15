472ee2bfdde7e3e6f9d76c6ed3a8bfc7762bf5a7
Kaylee McHugh <kayleemchugh@Kaylees-MacBook-Pro.local>
2016-02-15 13:40:13 -0700

    add exercise 20

---
+++ CollegeAdmission.java
@@ -0,0 +1,25 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class CollegeAdmission {
+	public static void main( String [] args ) {
+		Scanner keyboard = new Scanner(System.in);
+		int math;
+
+		out.println( "Welcome to the UT Austin College Admissions Interface!" );
+		out.print( "Please enter your SAT math score (200-800): " );
+		math = keyboard.nextInt();
+
+		out.print( "Admittance status: " );
+
+		if ( math >= 790 )
+			out.print( "CERTAIN " );
+		else if ( math >= 710 )
+			out.print( "SAFE " );
+		else if ( math >= 580 )
+			out.print( "PROBABLE ");
+		else // below 390
+		out.print( "DENIED ");
+		
+	}
+}
